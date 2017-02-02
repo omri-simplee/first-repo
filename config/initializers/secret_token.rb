f86885952639848cd5aadbd34b169e5171dca352
omri-simplee <omri@simplee.com>
2017-02-01 16:49:21 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0cb96f051b5cba9d69912cfdcd7bc5e73e8134cbace3dd11586a2ea349018ed2a6364e8903e964393102c7b5a6e6ef8b8515a1e0c4eea6f9156d7014751acb45'
